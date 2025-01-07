d49f5aa7fa6ddf472ef4661ce85622adaeefb6ea
yimje <imjy2@naver.com>
2025-01-07 17:55:53 +0900

    Initial commit

---
+++ src/main/java/study/chatstudy/ChatStudyApplication.java
@@ -0,0 +1,13 @@
+package study.chatstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/chatstudy/ChatStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.chatstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
